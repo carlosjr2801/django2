9f79b5303c898a86afa35d9c4b4cea8c81aa4410
Carlos Alberto <carlos.junior2801@gmail.com>
2024-12-14 23:12:36 -0300

    Primeiro commit do projeto Django

---
+++ core/static/js/script.js
@@ -0,0 +1,4 @@
+// Interação para expandir e recolher a barra lateral
+document.getElementById('menu-toggler').addEventListener('click', function() {
+  document.querySelector('.sidebar').classList.toggle('expanded');
+});
---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+from .views import index, contato
+
+
+urlpatterns = [
+   path('', index),
+   path('contato', contato)
+]
